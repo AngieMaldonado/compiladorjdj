1e3c769832b9118d47782e23233bbabaf3cdbe00
Jorge <JORZATE@gmail.com>
2013-10-04 11:29:50 -0500

    Clase BloqueParametro terminada.

--- Compilador/src/co/edu/uniquindio/analizadorSintactico/logic/BloqueParametro.java
+++ Compilador/src/co/edu/uniquindio/analizadorSintactico/logic/BloqueParametro.java
@@ -17,8 +17,54 @@ import java.util.ArrayList;
  * @version 1.1 Septiembre-2013 
  * Esta clase es la que contiene los atributos de BloqueParametro.java y 
  * maneja su información
- */
+ */ 
 public class BloqueParametro {
 
-	private ArrayList<Parametro>
+	/**
+	 * Atributo que contiene el valor de parametros dentro de la clase
+	*/
+	private ArrayList<Parametro> parametros;
+	
+	private Lenguaje idBloqueParametro;
+
+	/**
+	 * Metodo que se encarga de reservar memoria y luego instanciar la BloqueParametro.java
+	 */
+	public BloqueParametro(Lenguaje idBloqueParametro) 
+	{
+		super();
+		this.idBloqueParametro = idBloqueParametro;
+	}
+
+	/**
+	 * Este metodo permite obtener el valor del atributo parametros
+	 * @return el parametros
+	 */
+	public ArrayList<Parametro> getParametros() {
+		return parametros;
+	}
+
+	/**
+	 * Este metodo permite asignar un valor al atributo parametros
+	 * @param parametros se asigna a parametros
+	 */
+	public void setParametros(ArrayList<Parametro> parametros) {
+		this.parametros = parametros;
+	}
+
+	/**
+	 * Este metodo permite obtener el valor del atributo idBloqueParametro
+	 * @return el idBloqueParametro
+	 */
+	public Lenguaje getIdBloqueParametro() {
+		return idBloqueParametro;
+	}
+
+	/**
+	 * Este metodo permite asignar un valor al atributo idBloqueParametro
+	 * @param idBloqueParametro se asigna a idBloqueParametro
+	 */
+	public void setIdBloqueParametro(Lenguaje idBloqueParametro) {
+		this.idBloqueParametro = idBloqueParametro;
+	}
 }
